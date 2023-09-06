11ca0957662be28d60024e040996d4509e0354fc
Doane Marie I. Horlador <143169196+DoaneMIH@users.noreply.github.com>
2023-09-06 14:27:38 +0800

    Update QuestionNo7.java

--- QuestionNo7.java
+++ QuestionNo7.java
@@ -12,39 +12,51 @@ public class QuestionNo7 {
 			
 			
 			switch(month) {
+					//Months with 31 days
 				case(1):
 					System.out.print("January " + year + " has 31 days");
+					break;
 				case(3):
 					System.out.print("March " + year + " has 31 days");
+					break;
 				case(5):
 					System.out.print("May " + year + " has 31 days");
+					break;
 				case(7):
 					System.out.print("July " + year + " has 31 days");
+					break;
 				case(8):
 					System.out.print("August " + year + " has 31 days");
+					break;
 				case(10):
 					System.out.print("October " + year + " has 31 days");
+					break;
 				case(12):
 					System.out.print("December " + year + " has 31 days");
-				break;
-				
+					break;
+
+					//Months with 30 days
 				case(4):
 					System.out.print("April " + year + " has 30 days");
+					break;
 				case(6):
 					System.out.print("June " + year + " has 30 days");
+					break;
 				case(9):
 					System.out.print("September " + year + " has 30 days");
+					break;
 				case(11):
 					System.out.print("November " + year + " has 30 days");
-				break;
-				
+					break;
+
+					//February 
 				case(2):
 					if ((year % 4 == 0) && (year % 100 != 0)|| (year % 400 == 0)) {
 						System.out.print("February " + year + " has 29 days");
 						return;
 					} 
 						System.out.print("February " + year + " has 28 days");
-				break;
+					break;
 			}
 			
 	}
